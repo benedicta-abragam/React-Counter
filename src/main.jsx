3a22ab0c4430abc8560eb7c2f97de15f9a2506f4
benedicta-abragam <benedictaabraham079@gmail.com>
2026-01-03 01:16:41 +0530

    changes updateded

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Components from './components'
+import Components from './Components'
 
 createRoot(document.getElementById('root')).render(
   <>
